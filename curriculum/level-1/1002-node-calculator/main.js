9335504b0f93a8f0012fd1ae6097de858bfc0a6a
Ryland Bowles <rb@wlkmmas.org>
2024-08-20 09:55:42 -0400

    Creating...

---
+++ curriculum/level-1/1002-node-calculator/main.js
@@ -0,0 +1,26 @@
+const readline = require("readline-sync")
+
+function addition(a, b){
+    return a + b;
+}
+console.log(addition(4, 2));
+
+
+function createSum(a, b){
+    return a * b;
+}
+console.log(createSum(8, 7));
+
+
+function division(a, b){
+    return a / b;
+}
+console.log(division(25, 5));
+
+
+
+function subtraction(a, b){
+    return a - b;
+}
+console.log(subtraction(56, 14));
+
